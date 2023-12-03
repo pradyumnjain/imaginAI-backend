791eda43f94b8556d6c9bf18155dbfea93df291e
pradyumnjain25 <pradyumn@staple.io>
2023-12-03 19:07:41 +0530

    hello message

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, from github k !!!"
+    return "Hello, kanika from github !!!"
 
 
 @app.route("/api/data", methods=["GET"])
